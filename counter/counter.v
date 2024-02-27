e1dc238cba82a1a0f13780c51445875dca1abfd0
BhattSoham <98672977+BhattSoham@users.noreply.github.com>
2024-02-27 18:20:05 -0500

    Create counter.v

---
+++ counter/counter.v
@@ -0,0 +1,16 @@
+
+module counter(clk, rst, q);
+ input clk, rst;
+ output [3:0] q;
+
+ wire clk, rst;
+ reg [3:0] q;
+
+always @ (posedge clk)
+ begin
+     if(rst)
+       q = 0;
+     else
+        q = q + 1;  
+ end
+endmodule 
